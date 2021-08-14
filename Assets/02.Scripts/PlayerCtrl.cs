61d1b3db1d8873334d00c9d608a64c12eac2b053
Lee Jae Hyun <myleje@gmail.com>
2021-08-14 17:01:50 +0900

    Project upload

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    void Awake()
+    {
+
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // 렌더링 주기 30FPS = 1/30 sec
+    void Update()
+    {
+        //
+    }
+
+    // 물리엔진의 계산주기 0.02f
+    void FixedUpdate()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        //
+    }
+}
