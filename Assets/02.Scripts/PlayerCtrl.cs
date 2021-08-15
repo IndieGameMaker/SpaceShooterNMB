14c91a84364ca7aee0177ab83c75fea07c7edd1a
Lee Jae Hyun <myleje@gmail.com>
2021-08-15 11:21:54 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,7 +6,11 @@ public class PlayerCtrl : MonoBehaviour
 {
     public Transform tr;
 
-    private float h, v;
+    private float h, v, r;
+    [System.NonSerialized]
+    public float moveSpeed = 6.0f;
+    [HideInInspector]
+    public float turnSpeed = 100.0f;
 
     void Start()
     {
@@ -16,8 +20,15 @@ public class PlayerCtrl : MonoBehaviour
     // 렌더링 주기 30FPS = 1/30 sec
     void Update()
     {
-        //tr.position += new Vector3(0, 0, 0.1f);
-        tr.position += Vector3.forward * 0.1f;
+        v = Input.GetAxis("Vertical");  // -1.0f ~ 0.0f ~ +1.0f
+        h = Input.GetAxis("Horizontal");// -1.0f ~ 0.0f ~ +1.0f
+        r = Input.GetAxis("Mouse X");
+
+        // 이동처리
+        Vector3 dir = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(dir.normalized * Time.deltaTime * moveSpeed);
+        // 회전처리
+        tr.Rotate(Vector3.up * Time.deltaTime * turnSpeed * r);
     }
 
     /*
