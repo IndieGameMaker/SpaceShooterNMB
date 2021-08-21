b3dcde726893040d45906f7cbabeca3964a41da7
Lee Jae Hyun <myleje@gmail.com>
2021-08-21 16:48:14 +0900

    쿼터니언 변환

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -20,6 +20,40 @@ public class RemoveBullet : MonoBehaviour
         {
             //총알을 삭제
             Destroy(coll.gameObject);
+
+            // 충돌 지점(좌표) 추출
+            Vector3 pos = coll.GetContact(0).point;
+            // 법선 벡터(Normal Vector)
+            Vector3 _normal = -coll.GetContact(0).normal;
+            // 법선 벡터의 각도를 쿼터니언 타입으로 변환
+            Quaternion rot = Quaternion.LookRotation(_normal);
+
+            // 스파크 이펙트를 생성
+            Instantiate(sparkEffect, pos, rot);
         }
     }
 }
+
+/*
+    쿼터니언 (Quaternion) : 사원수  (x, y, z, w) : 복소수 사차원 벡터
+
+    오일러 회전(Euler) 0~360 
+    x -> y -> z
+
+    * Gimbal Lock (짐벌락, 김벌락)
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
