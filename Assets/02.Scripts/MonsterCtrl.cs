8d3eb91e0c93dde67fdcaf1bc24acf8131f0ce2a
Lee Jae Hyun <myleje@gmail.com>
2021-08-21 17:44:21 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;  // NavMeshAgent 컴포넌트
 
 public class MonsterCtrl : MonoBehaviour
 {
     private Transform playerTr;
+    private NavMeshAgent agent;
 
     void Start()
     {
         //GameObject playerObject = GameObject.Find("Player");
         GameObject playerObject = GameObject.FindGameObjectWithTag("PLAYER");
         playerTr = playerObject.GetComponent<Transform>();
-
+        agent = GetComponent<NavMeshAgent>();
         //playerTr = GameObject.FindGameObjectWithTag("PLAYER")?.GetComponent<Transform>();
     }
 
